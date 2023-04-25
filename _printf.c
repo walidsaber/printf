83fbbe08c7d1052eeb845a3e05f883244a400b5f
walidsaber <walid_saber123@outlook.com>
2023-04-25 09:10:25 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,49 +1,58 @@
-#include <stdio.h>
 #include "main.h"
-#include <stdarg.h>
+
 /**
- * _printf - printf replica
- * @format: format type
- * @...: args
- * Return: count
+ * _printf - Printf replica
+ * @format: format.
+ * Return: Printed chars.
  */
+
 int _printf(const char *format, ...)
 {
-	const char *ptr;
-	va_list n_list;
-	int count = 0;
+	int i, print = 0, print_c = 0, fl, wid, prec, sz, buffer2 = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	va_start(n_list, format);
-	for (ptr = format; *ptr; ptr++)
+	if (format == NULL)
+		return (-1);
+	va_start(list, format);
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (*ptr != '%')
+		if (format[i] != '%')
 		{
-			putchar(*ptr);
-			count++;
-			continue;
+			buffer[buffer2++] = format[i];
+			if (buffer2 == BUFF_SIZE)
+				print_buffer(buffer, &buffer2);
+			print_c++;
 		}
-		switch (*++ptr)
+		else
 		{
-			case 'i':
-			case 'd':
-				print_int(va_arg(n_list, int));
-				count++;
-				break;
-			case 's':
-				count += print_str(va_arg(n_list, char *));
-				break;
-			case 'c':
-				putchar((char)va_arg(n_list, int));
-				count++;
-				break;
-			default:
-				putchar('%');
-				if ((*ptr) != '%')
-					putchar(*ptr);
-				count++;
-				break;
+			print_buffer(buffer, &buffer2);
+			fl = get_flags(format, &i);
+			wid = get_width(format, &i, list);
+			prec = get_precision(format, &i, list);
+			sz = get_size(format, &i);
+			++i;
+			print = handle_print(format, &i, list, buffer,
+					fl, wid, prec, sz);
+			if (print == -1)
+				return (-1);
+			print_c += print;
 		}
 	}
-	va_end(n_list);
-	return (count);
+	print_buffer(buffer, &buffer2);
+	va_end(list);
+	return (print_c);
+}
+
+/**
+ * print_buffer - prints out the buffer
+ * @buffer: arr
+ * @buffer2: add
+ */
+
+void print_buffer(char buff[], int *buffer2)
+{
+	if (*buffer2 > 0)
+		write(1, &buff[0], *buffer2);
+	*buffer2 = 0;
 }
