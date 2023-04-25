f00bfcec73f7324123b7d508a90a60185c76f20b
walidsaber <walid_saber123@outlook.com>
2023-04-25 12:06:30 -0700

    printf

--- print_rev.c
+++ print_rev.c
@@ -9,15 +9,15 @@
 
 int print_rev(va_list l, sign_t *f)
 {
-    int i = 0, k;
-    char *s = va_arg(l, char *);
+	int i = 0, k;
+	char *s = va_arg(l, char *);
 
-    (void)f;
-    if (!s)
-        s = "(null)";
-    while (s[i])
-        i++;
-    for (k = i - 1; k >= 0; k--)
-        _putchar(s[k]);
-    return (i);
+	(void)f;
+	if (!s)
+		s = "(null)";
+	while (s[i])
+		i++;
+	for (k = i - 1; k >= 0; k--)
+		_putchar(s[k]);
+	return (i);
 }
